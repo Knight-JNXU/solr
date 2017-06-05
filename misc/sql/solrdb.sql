3acb84e422dfd0aed702e0182b493ca3fba8082a
wen.liu <wen.liu@baozun.cn>
2017-06-05 18:31:00 +0800

    sql update

--- misc/sql/solrdb.sql
+++ misc/sql/solrdb.sql
@@ -1,26 +1,34 @@
-create database solrdb;
+/*建表插入数据*/
 
-create sequence serial_id increment by 1 minvalue 1 no maxvalue start with 1;
+create sequence item_category_serial_id increment by 1 minvalue 1 no maxvalue start with 1;
+create sequence item_serial_id increment by 1 minvalue 1 no maxvalue start with 1;
+create sequence category_serial_id increment by 1 minvalue 1 no maxvalue start with 1;
 
 create table t_item_category
 (
-	id int8 not null primary key default nextval('serial_id'),
+	id int8 not null primary key default nextval('item_category_serial_id'),
 	item_id int8,
 	category_id int8
 );
 create table t_item
 (
-	id int8 not null primary key,
+	id int8 not null primary key default nextval('item_serial_id'),
 	name varchar(256)
 );
 create table t_category
 (
-	id int8 not null primary key,
+	id int8 not null primary key default nextval('category_serial_id'),
 	name varchar(256)
 );
 
 insert into t_item_category(item_id, category_id) values(1, 1);
 insert into t_item_category(item_id, category_id) values(2, 1);
-insert into t_item values(1, 'Reebok GL 6000 ND');
-insert into t_item values(2, 'Reebok AZTEC RETRO ');
-insert into t_category values(1, '鞋 ');
+insert into t_item_category(item_id, category_id) values(3, 2);
+insert into t_item_category(item_id, category_id) values(4, 3);
+insert into t_item(name) values('Reebok GL 6000 ND');
+insert into t_item(name) values('Reebok AZTEC RETRO');
+insert into t_item(name) values('Reebok WOR WVN JACKET');
+insert into t_item(name) values('Reebok EL BLUEY COLLAB SHORT');
+insert into t_category(name) values('鞋 ');
+insert into t_category(name) values('衣服 ');
+insert into t_category(name) values('裤子');
